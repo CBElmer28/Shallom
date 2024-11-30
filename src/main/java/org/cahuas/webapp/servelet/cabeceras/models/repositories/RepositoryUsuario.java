067edce9ca916508049f3541217d36092ee09b55
Jonathan Soto <157034466+Jhon3125@users.noreply.github.com>
2024-11-30 16:41:54 -0500

    Update RepositoryUsuario.java

--- src/main/java/org/cahuas/webapp/servelet/cabeceras/models/repositories/RepositoryUsuario.java
+++ src/main/java/org/cahuas/webapp/servelet/cabeceras/models/repositories/RepositoryUsuario.java
@@ -6,21 +6,17 @@ package org.cahuas.webapp.servelet.cabeceras.models.repositories;
 
 import java.sql.SQLException;
 
-/**
- *
- * @author keyen
- */
 public interface RepositoryUsuario<T> {
-     T UsuarioporId(int id) throws SQLException;
-     
-     T UsuarioSesion(String username,String password) throws SQLException;
-     
-     T existeUsuario(String username) throws SQLException;
-     
-     void crearCuenta(int dni,String usuario,String pass,String tipo)throws SQLException;
-     
-      void editarCuenta(int dni,String usuario,String pass,String tipo)throws SQLException;
-      
-      void eliminarUsuario(int dni)throws SQLException;
-     
+    T UsuarioporId(int id) throws SQLException;
+
+    T UsuarioSesion(String username,String password) throws SQLException;
+
+    T existeUsuario(String username) throws SQLException;
+
+    void crearCuenta(int dni,String usuario,String pass,String tipo)throws SQLException;
+
+    void editarCuenta(int id,int dni,String usuario,String pass,String tipo)throws SQLException;
+
+    void eliminarUsuario(int dni)throws SQLException;
+
 }
