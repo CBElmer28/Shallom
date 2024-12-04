66a7cf8c0221c3d568b43c0358591872f6fa2917
D-Leon-P <jeff.pao13@gmail.com>
2024-12-03 22:24:29 -0500

    Update LogoutServlet.java

--- src/main/java/org/cahuas/webapp/servelet/cabeceras/controlador/LogoutServlet.java
+++ src/main/java/org/cahuas/webapp/servelet/cabeceras/controlador/LogoutServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.cahuas.webapp.servelet.cabeceras.controlador;
 
 import jakarta.servlet.ServletException;
@@ -14,19 +10,38 @@ import java.io.IOException;
 import java.util.Optional;
 import org.cahuas.webapp.servelet.cabeceras.models.services.LoginService;
 
-
 /**
+ * Servlet que maneja el proceso de cierre de sesión de un usuario.
+ * Este servlet invalida la sesión activa del usuario y redirige al usuario a la página de inicio.
+ * 
+ * @author Team Shalom
+ * @version 1.6
  *
- * @author keyen
  */
 @WebServlet("/logout")
-public class LogoutServlet extends HttpServlet{
+public class LogoutServlet extends HttpServlet {
+    
+    /**
+     * Maneja la solicitud HTTP GET para cerrar la sesión del usuario.
+     * Este método invalida la sesión activa del usuario, eliminando todos los atributos de la sesión,
+     * y redirige al usuario a la página de inicio.
+     * 
+     * @param req la solicitud HTTP que contiene la información de la sesión.
+     * @param resp la respuesta HTTP que se envía al cliente después de cerrar sesión.
+     * @throws ServletException si ocurre un error relacionado con el servlet durante el procesamiento de la solicitud.
+     * @throws IOException si ocurre un error durante el proceso de redirección o escritura en la respuesta.
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Obtiene la sesión actual si existe
         HttpSession session = req.getSession(false);
+        
+        // Si la sesión existe, la invalida
         if (session != null) {
             session.invalidate();
         }
+        
+        // Redirige al usuario a la página de inicio
         resp.sendRedirect(req.getContextPath() + "/usuario/index.jsp");
     }
 }
